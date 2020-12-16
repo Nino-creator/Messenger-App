9598e147ca8a565ed01ee9723f23bfe933744ffd
Nino-creator <nino.mekhashishvili15b@geolab.edu.ge>
2020-12-16 20:38:59 +0200

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Nini mekhashishvili on 12/16/20.
+//  Copyright © 2020 niniko mekhashishvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
